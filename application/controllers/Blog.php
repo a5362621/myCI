cdfe5e34ff2779bfdf3b70b57a8a8db1e183a128
a5362621 <244970635@qq.com>
2018-02-22 17:15:51 +0800

    minxindd

---
+++ application/controllers/Blog.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BER
+ * Date: 2018/2/22
+ * Time: 17:14
+ */
+class Blog extends CI_Controller{
+    public function index(){
+        echo 'helloworld';
+    }
+}
